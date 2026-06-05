b8a78f3ca180dc20482a28bcc069ec30c30a32ba
Sergey <sergeydergynov038.uniads@gmail.com>
2026-06-05 14:31:41 +0300

    ST-2: fix 3

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -6,16 +6,16 @@
 double calculateEarthRopeGap() {
     const double earthRadiusMeters = 6378100.0;
     const double addedLength = 1.0;
-    
+
     Circle oldEarth(earthRadiusMeters);
     double oldFerence = oldEarth.getFerence();
-    
+
     double newFerence = oldFerence + addedLength;
-    
+
     Circle newEarth(0.0);
     newEarth.setFerence(newFerence);
     double newRadius = newEarth.getRadius();
-    
+
     return newRadius - earthRadiusMeters;
 }
 
@@ -24,18 +24,16 @@ PoolCost calculatePoolCosts() {
     const double pathWidth = 1.0;
     const double concretePrice = 1000.0;
     const double fencePrice = 2000.0;
-    
+
     Circle pool(poolRadius);
-    
     Circle outer(poolRadius + pathWidth);
-    
+
     double pathArea = outer.getArea() - pool.getArea();
-    
     double fenceLength = outer.getFerence();
-    
+
     PoolCost costs;
     costs.concrete = pathArea * concretePrice;
     costs.fence = fenceLength * fencePrice;
-    
+
     return costs;
 }
